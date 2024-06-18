998065e8634aaab55b4b013410109368e8420a7b
M0rT4r <notan@email.com>
2024-06-18 17:28:51 -0300

    A WINDOW

--- graphics.cpp
+++ graphics.cpp
@@ -1,13 +1,21 @@
 #include "graphics.h"
 #include <SDL2/SDL.h>
-#define W 500
-#define H 500
+#include <stdio.h>
+#define W 500 // Window width
+#define H 500 // Window height
 
 Graphics::Graphics(){
-    SDL_CreateWindowAndRenderer(W,H,0,
-                                &this->_window,
-                                &this->_renderer
-                                );
+    this->_window = SDL_CreateWindow("title",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,W,H,0);
+    if(this->_window == NULL){
+        printf("Failed to create a window.\n");
+    }
+    this->_renderer = SDL_CreateRenderer(this->_window,-1,0);
+    if(this->_renderer== NULL){
+        printf("Failed to create a renderer for the window.\n");
+    }
+    SDL_SetRenderDrawColor(this->_renderer,0,100,0,255);
+    SDL_RenderClear(this->_renderer);
+    SDL_RenderPresent(this->_renderer);
 }
 
 Graphics::~Graphics(){}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "game.h"
 #include "graphics.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
 int main(){
     //Game game;
-    Graphics graphics;
-    while(true){
+    if(SDL_Init(SDL_INIT_VIDEO)){
+        printf("Couldn't init SDL video.\n");
+        return -1;
     }
+    printf("SDL error says:\n\t-%s\n",SDL_GetError());
+    Graphics graphics;
+    bool running = true;
+    SDL_GetError();
+    while (running) {
+    }
+    
+    printf("Exiting.\n");
+    return 0;
 }
